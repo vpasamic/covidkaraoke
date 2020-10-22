637c3b3238c578c09493396321662cc9d924ebc9
Nilou Mostofi <nilou@km-direct.com>
2020-10-22 14:49:15 -0700

    tested api-routes

--- routes/search-api-routes.js
+++ routes/search-api-routes.js
@@ -5,33 +5,23 @@ module.exports = app => {
   // GET route for getting all of the posts
   app.get("/api/history", (req, res) => {
     const query = {};
-    if (req.query.user_id) {
-      query.UserId = req.query.user_id;
-    }
-    console.log("start");
-    // Here we add an "include" property to our options in our findAll query
-    db.Search.findAll({
-      where: query,
-      include: [db.User]
-      // limit: 50,
-      // order: [["createdAt", "DESC"]]
-    }).then(dbSearch => {
-      console.log("dbSearch");
-      res.json(dbSearch);
-    });
     if (!req.user) {
-      // The user is not logged in, send back an empty object
       res.json({});
     } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
+      console.log("start");
+      // Here we add an "include" property to our options in our findAll query
+      db.Search.findAll({
+        where: {
+          UserId: req.user.id
+        },
+        limit: 50,
+        order: [["createdAt", "DESC"]]
+      }).then(dbSearch => {
+        console.log("dbSearch");
+        res.json(dbSearch);
       });
     }
   });
-
   // POST route for saving a new post
   app.post("/api/history", (req, res) => {
     db.Search.create(req.body).then(dbSearch => {
